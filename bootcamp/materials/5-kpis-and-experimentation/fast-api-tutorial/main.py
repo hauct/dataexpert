7647a1814bb2912a78a6cb8dad3307112122e02b
cungtronghau <cungtronghau@gmail.com>
2025-02-24 18:18:24 +0700

    edit

--- bootcamp/materials/5-kpis-and-experimentation/fast-api-tutorial/main.py
+++ bootcamp/materials/5-kpis-and-experimentation/fast-api-tutorial/main.py
@@ -1,45 +1,119 @@
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, status
 from enum import Enum
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 app = FastAPI()
 
-@app.get("/", description='This is our first route')
-async def base_get_root():
-    return {'message': 'Hello World, I am Hau, i am an data hello'}
+# @app.get("/", description='This is our first route')
+# async def base_get_root():
+#     return {'message': 'Hello World, I am Hau, i am an data hello'}
 
-@app.get('/greet')
-async def greet_name(name: Optional[str] = 'User', age:int = 0) -> dict:
-    return {'message': f'Hello {name}', 'age': age}
+# @app.get('/greet')
+# async def greet_name(name: Optional[str] = 'User', age:int = 0) -> dict:
+#     return {'message': f'Hello {name}', 'age': age}
 
-class BookCreateModel(BaseModel):
+# class BookCreateModel(BaseModel):
+#     title: str
+#     author: str
+
+# @app.post('/create_book')
+# async def create_book(book_data: BookCreateModel):
+#     return {
+#         'title': book_data.title
+#         , 'author': book_data.author
+#     }
+
+# @app.get('/get_headers')
+# async def get_headers(accept:str = Header(None)
+#                       , content_type:str = Header(None)
+#                       , user_agent:str = Header(None)
+#                       , host:str = Header(None)
+#                     ):
+#     request_headers = {}
+#     request_headers['Accept'] = accept
+#     request_headers['Content-Type'] = content_type
+#     request_headers['User-Agent'] = user_agent
+#     request_headers['Host'] = host
+
+#     return request_headers
+
+
+books = [
+    {
+        "id": 1,
+        "title": "Think Python",
+        "author": "Allen B. Downey",
+        "publisher": "O'Reilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 2,
+        "title": "Django By Example",
+        "author": "Antonio Mele",
+        "publisher": "Packt Publishing Ltd",
+        "published_date": "2022-01-19",
+        "page_count": 1023,
+        "language": "English",
+    },
+    {
+        "id": 3,
+        "title": "The web socket handbook",
+        "author": "Alex Diaconu",
+        "publisher": "Xinyu Wang",
+        "published_date": "2021-01-01",
+        "page_count": 3677,
+        "language": "English",
+    },
+    {
+        "id": 4,
+        "title": "Head first Javascript",
+        "author": "Hellen Smith",
+        "publisher": "Oreilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 540,
+        "language": "English",
+    },
+    {
+        "id": 5,
+        "title": "Algorithms and Data Structures In Python",
+        "author": "Kent Lee",
+        "publisher": "Springer, Inc",
+        "published_date": "2021-01-01",
+        "page_count": 9282,
+        "language": "English",
+    },
+    {
+        "id": 6,
+        "title": "Head First HTML5 Programming",
+        "author": "Eric T Freeman",
+        "publisher": "O'Reilly Media",
+        "published_date": "2011-21-01",
+        "page_count": 3006,
+        "language": "English",
+    },
+]
+
+class Book(BaseModel):
+    id: int
     title: str
     author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
 
-@app.post('/create_book')
-async def create_book(book_data: BookCreateModel):
-    return {
-        'title': book_data.title
-        , 'author': book_data.author
-    }
+@app.get('/books', response_model=List[Book])
+async def get_all_books():
+    return books
 
-@app.get('/get_headers')
-async def get_headers(accept:str = Header(None)
-                      , content_type:str = Header(None)
-                      , user_agent:str = Header(None)
-                      , host:str = Header(None)
-                    ):
-    request_headers = {}
-    request_headers['Accept'] = accept
-    request_headers['Content-Type'] = content_type
-    request_headers['User-Agent'] = user_agent
-    request_headers['Host'] = host
-
-    return request_headers
-
-
-
+@app.post('/books', status_code=status.HTTP_201_CREATED)
+async def create_a_book(book_data:Book)-> dict:
+    new_book = book_data.model_dump()
+    books.append(new_book)
+    return new_book
 
 # @app.post("/")
 # async def post():
